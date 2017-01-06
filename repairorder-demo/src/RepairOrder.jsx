037a8aaa8d4c69c1e7cc6db5ffac88fc5a95dcc8
xwangq123 <xwangq123@163.com>
2017-01-06 17:03:58 +0800

    提交代码

--- repairorder-demo/src/RepairOrder.jsx
+++ repairorder-demo/src/RepairOrder.jsx
@@ -12,11 +12,29 @@ class RepairOrder extends Component {
         super(props)
     }
 
+
     render() {
+        const rowSelection = {
+            onChange: (selectedRowKeys, selectedRows) => {
+                console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+            },
+            onSelect: (record, selected, selectedRows) => {
+                console.log(record, selected, selectedRows);
+            },
+            onSelectAll: (selected, selectedRows, changeRows) => {
+                console.log(selected, selectedRows, changeRows);
+            },
+            getCheckboxProps: record => ({
+                disabled: record.name === 'Disabled User',
+            }),
+        };
+
         return (
             <Content style={{padding: '0 24px'}}>
-                <Panel query={this.props.query}></Panel>
-                <Table dataSource={this.props.dataSource}
+                <Panel query={this.props.query}>
+                </Panel>
+                <Table rowSelection={rowSelection}
+                       dataSource={this.props.dataSource}
                        columns={this.props.columns}>
                 </Table>
             </Content>
--- repairorder-demo/src/action.js
+++ repairorder-demo/src/action.js
@@ -1,8 +1,9 @@
+import fetch from 'isomorphic-fetch';
+
 export const QUERY = 'QUERY';
 export const ERROR = 'ERROR';
 
 const query = filter => dispatch => {
-    const fetch = require('isomorphic-fetch');
     fetch('../data/repairOrder.json?')
         .then(r => r.json())
         .then(result => dispatch({
